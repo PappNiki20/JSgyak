cf601b6a27e3aa60248cefa1098e898c75370a65
PappNiki20 <papp.nikoletta001@gmail.com>
2023-03-09 23:07:10 +0100

    a kepek griddel rendezve, viszont !  egymas mellet

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 window.addEventListener("load", function () {
   const ARTICLEELEM = document.querySelectorAll("article");
+  ARTICLEELEM[0].classList.add("container");
   const lista = [
-    "acotar",
-    "acotar2 ",
+    "acotar5",
+    "acotar4",
+    "acotar2",
+    "acotar3",
+    "acotar2",
+    "acotar5",
     "acotar3",
     "acotar",
-    "acotar2 ",
-    "acotar3",
   ];
   console.log(lista[0]);
- 
+  
   for (let index = 0; index < 8; index++) {
     ARTICLEELEM[0].innerHTML += `<div><img src='${lista[index]}.jpg' alt='' ></div>`;
+  
   }
+  
+  const DIVELEM = document.querySelectorAll("div");
+  for (let index = 0; index < lista.length; index++) {
+    DIVELEM[index].classList.add("kep");
+  }
+ 
 });
