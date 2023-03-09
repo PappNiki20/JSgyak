e077c381b6757b3c667063a4592bf9632ac0c1f0
PappNiki20 <papp.nikoletta001@gmail.com>
2023-03-09 16:36:55 +0100

    megjelennek a kepek, otthon befejezem

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 window.addEventListener("load", function () {
-    const ARTICLEELEM = document.querySelectorAll("article");
-    const lista=["acotar2, acotar3, acotar4, acotar5, acotar, acotar2, acotar3"];
-    for (let index = 0; index < 8; index++) {
-        ARTICLEELEM.innerHTML = `<div><img src="${lista[index]}.jpg" alt='' ></div>`;
-        
-    }
+  const ARTICLEELEM = document.querySelectorAll("article");
+  const lista = [
+    "acotar",
+    "acotar2 ",
+    "acotar3",
+    "acotar",
+    "acotar2 ",
+    "acotar3",
+  ];
+  console.log(lista[0]);
+ 
+  for (let index = 0; index < 8; index++) {
+    ARTICLEELEM[0].innerHTML += `<div><img src='${lista[index]}.jpg' alt='' ></div>`;
+  }
 });
