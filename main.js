5ac6d8c7bb1a32afb3e4cb424346e6b9255dee8f
PappNiki20 <papp.nikoletta001@gmail.com>
2023-03-10 11:12:59 +0100

    A kepgaleria formazasa scriptje kesz

--- main.js
+++ main.js
@@ -1,26 +1,46 @@
+let SZAMLALO = 0;
 window.addEventListener("load", function () {
   const ARTICLEELEM = document.querySelectorAll("article");
   ARTICLEELEM[0].classList.add("container");
   const lista = [
-    "acotar5",
-    "acotar4",
-    "acotar2",
-    "acotar3",
-    "acotar2",
-    "acotar5",
-    "acotar3",
-    "acotar",
+    "acotar2.jpg",
+    "acotar0.jpg",
+    "acotar4.jpg",
+    "acotar3.jpg",
+    "acotar0.jpg",
+    "acotar3.jpg",
+    "acotar4.jpg",
+    "acotar0.jpg",
   ];
   console.log(lista[0]);
-  
-  for (let index = 0; index < 8; index++) {
-    ARTICLEELEM[0].innerHTML += `<div><img src='${lista[index]}.jpg' alt='' ></div>`;
-  
+
+  for (let index = 0; index < 4; index++) {
+    ARTICLEELEM[0].innerHTML += `<div><img src='${lista[index]}' alt='' ></div>`;
   }
-  
-  const DIVELEM = document.querySelectorAll("div");
-  for (let index = 0; index < lista.length; index++) {
-    DIVELEM[index].classList.add("kep");
-  }
- 
+
+  const GombJobbra = document.querySelectorAll(".jobb");
+  GombJobbra[0].addEventListener("click", function () {
+    const NAGYKEP = document.querySelectorAll(".magykep img");
+    NAGYKEP[0].style.width = "100%";
+
+    NAGYKEP[0].src = lista[SZAMLALO];
+    if (SZAMLALO >= 7) {
+      NAGYKEP[0].src = lista[0];
+      SZAMLALO = 0;
+    }
+    SZAMLALO += 1;
+  });
+
+  const GombBalra = document.querySelectorAll(".bal");
+  GombBalra[0].addEventListener("click", function () {
+    const NAGYKEP = document.querySelectorAll(".magykep img");
+    NAGYKEP[0].style.width = "100%";
+
+    NAGYKEP[0].src = lista[SZAMLALO ];
+    if (SZAMLALO >= 7) {
+      NAGYKEP[0].src = lista[0];
+      SZAMLALO = 0;
+    }
+    SZAMLALO += 1;
+  });
 });
