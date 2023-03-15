986e2510073f37a05b1ea36d4e5e432eb98fc749
PappNiki20 <papp.nikoletta001@gmail.com>
2023-03-15 23:36:42 +0100

    uj kepekkel, js befejezve

--- main.js
+++ main.js
@@ -3,19 +3,30 @@ window.addEventListener("load", function () {
   const ARTICLEELEM = document.querySelectorAll("article");
   ARTICLEELEM[0].classList.add("container");
   const lista = [
-    "acotar2.jpg",
-    "acotar0.jpg",
-    "acotar4.jpg",
-    "acotar3.jpg",
-    "acotar0.jpg",
-    "acotar3.jpg",
-    "acotar4.jpg",
-    "acotar0.jpg",
+    "F1Kicsi.jpg",
+    "F3Kicsi.jpg",
+    "F4Kicsi.jpg",
+    "F5Kicsi.jpg",
+    "F1Kicsi.jpg",
+    "F2Kicsi.jpg",
+    "F3Kicsi.jpg",
+    "F4Kicsi.jpg",
+  ];
+  const listaN = [
+    "F1Nagy.jpg",
+    "F3Nagy.jpg",
+    "F4Nagy.jpg",
+    "F5Nagy.jpg",
+    "F1Nagy.jpg",
+    "F2Nagy.jpg",
+    "F3Nagy.jpg",
+    "F4Nagy.jpg",
   ];
   console.log(lista[0]);
 
-  for (let index = 0; index < 4; index++) {
-    ARTICLEELEM[0].innerHTML += `<div><img src='${lista[index]}' alt='' ></div>`;
+  for (let index = 0; index < 8; index++) {
+    ARTICLEELEM[0].innerHTML += `<div class="item"><img src='${lista[index]}'alt='' ></div>`;
+    
   }
 
   const GombJobbra = document.querySelectorAll(".jobb");
@@ -23,9 +34,9 @@ window.addEventListener("load", function () {
     const NAGYKEP = document.querySelectorAll(".magykep img");
     NAGYKEP[0].style.width = "100%";
 
-    NAGYKEP[0].src = lista[SZAMLALO];
+    NAGYKEP[0].src = listaN[SZAMLALO];
     if (SZAMLALO >= 7) {
-      NAGYKEP[0].src = lista[0];
+      NAGYKEP[0].src = listaN[0];
       SZAMLALO = 0;
     }
     SZAMLALO += 1;
@@ -36,9 +47,9 @@ window.addEventListener("load", function () {
     const NAGYKEP = document.querySelectorAll(".magykep img");
     NAGYKEP[0].style.width = "100%";
 
-    NAGYKEP[0].src = lista[SZAMLALO ];
+    NAGYKEP[0].src = listaN[SZAMLALO ];
     if (SZAMLALO >= 7) {
-      NAGYKEP[0].src = lista[0];
+      NAGYKEP[0].src = listaN[0];
       SZAMLALO = 0;
     }
     SZAMLALO += 1;
